923040e65fa231df74151ead96a81c307bfee433
Perdomo Murcia <lfperdomo@indracompany.com>
2023-03-23 20:45:09 -0500

    avance crud

--- App.js
+++ App.js
@@ -1,139 +1,142 @@
 import { useState, useEffect } from "react";
-import {
-  Platform,
+import { 
   ScrollView,
   StyleSheet,
   Text,
   TextInput,
   TouchableOpacity,
   View,
+  Button
 } from "react-native";
 import Constants from "expo-constants";
 import * as SQLite from "expo-sqlite";
 
 function openDatabase() {  
-  const db = SQLite.openDatabase("test.db");
+  const db = SQLite.openDatabase("test4.db");
   return db;
 }
 
 const db = openDatabase();
+ 
 
-function Items({ done: doneHeading, onPressItem }) {
-  const [items, setItems] = useState(null);
+export default function App() { 
 
-  useEffect(() => {
-    db.transaction((tx) => {
-      tx.executeSql(
-        `select * from items where done = ?;`,
-        [doneHeading ? 1 : 0],
-        (_, { rows: { _array } }) => setItems(_array)
-      );
-    });
-  }, []);
+  const [items, setItems] = useState([]);
+  const [ showForm, setShowForm ] = useState(false);
+  const [ formData, setFormData ] = useState({
+    name: "Juan", address: "Cll 100", phone: 319028484
+  });
 
-  const heading = doneHeading ? "Completed" : "Todo";
-
-  if (items === null || items.length === 0) {
-    return null;
-  }
-
-  return (
-    <View style={styles.sectionContainer}>
-      <Text style={styles.sectionHeading}>{heading}</Text>
-      {items.map(({ id, done, value }) => (
-        <TouchableOpacity
-          key={id}
-          onPress={() => onPressItem && onPressItem(id)}
-          style={{
-            backgroundColor: done ? "#1c9963" : "#fff",
-            borderColor: "#000",
-            borderWidth: 1,
-            padding: 8,
-          }}
-        >
-          <Text style={{ color: done ? "#fff" : "#000" }}>{value}</Text>
-        </TouchableOpacity>
-      ))}
-    </View>
-  );
-}
-
-export default function App() {
-  const [text, setText] = useState(null);
   const [forceUpdate, forceUpdateId] = useForceUpdate();
 
   useEffect(() => {
     db.transaction((tx) => {
       tx.executeSql(
-        "create table if not exists items (id integer primary key not null, done int, value text);"
+        "create table if not exists items (id integer primary key not null, name text, address text, phone int);"
       );
+      reloadData(tx)
     });
   }, []);
 
-  const add = (text) => {
-    // is text empty?
-    if (text === null || text === "") {
-      return false;
-    }
+  const reloadData = (tx) => {
+    tx.executeSql( `select * from items;`, [ ],
+      (_, { rows: { _array } }) => setItems(_array)
+    );
+  }
+
+  const addDatabase = () => {
+    
+      db.transaction(
+        (tx) => {
+          tx.executeSql("insert into items (name, address, phone) values ( ?, ?, ?)", [
+            formData.name, formData.address, formData.phone
+          ]);
+          reloadData(tx);
+        },
+        null,
+        forceUpdate
+      );
+
+      setShowForm(false);
+  
+  };
+
+  const onClickDelete = (id) => {
 
     db.transaction(
       (tx) => {
-        tx.executeSql("insert into items (done, value) values (0, ?)", [text]);
-        tx.executeSql("select * from items", [], (_, { rows }) =>
-          console.log(JSON.stringify(rows))
-        );
+        tx.executeSql(`delete from items where id = ?;`, [id]);
+        reloadData(tx)
       },
       null,
       forceUpdate
-    );
-  };
+    )
+
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.heading}>SQLite Example</Text>
- 
+      <View style={{height:20}} />
+      <Text style={styles.heading}>SQLite CRUD Sample 2</Text>
+  
         <>
-          <View style={styles.flexRow}>
-            <TextInput
-              onChangeText={(text) => setText(text)}
-              onSubmitEditing={() => {
-                add(text);
-                setText(null);
-              }}
-              placeholder="what do you need to do?"
-              style={styles.input}
-              value={text}
-            />
-          </View>
+
+          {
+            showForm?
+            <View style={{ padding: 20 }}>
+
+              <TextInput
+                onChangeText={(text) => setFormData({ ...formData, 'name': text  })} 
+                placeholder="Jhon Doe"
+                style={styles.input}
+                value={formData.name}
+              /> 
+
+              <TextInput
+                onChangeText={(text) => setFormData({ ...formData, 'address': text  })} 
+                placeholder="Cll 100"
+                maxLength={100}
+                style={styles.input}
+                value={formData.address}
+              /> 
+
+              <TextInput
+                onChangeText={(text) => setFormData({ ...formData, 'phone': text  })} 
+                placeholder="300000000"
+                maxLength={10}
+                style={styles.input}
+                value={formData.phone+""}
+                inputMode={"numeric"}
+              /> 
+
+              <Button onPress={addDatabase} title="Save" color="#2E4053"  />
+            </View>
+            :
+            <View style={{ padding: 20 }}>
+              <Button onPress={()=>setShowForm(true)} title="New" color="#2E4053"  />
+            </View>
+          }
           <ScrollView style={styles.listArea}>
-            <Items
-              key={`forceupdate-todo-${forceUpdateId}`}
-              done={false}
-              onPressItem={(id) =>
-                db.transaction(
-                  (tx) => {
-                    tx.executeSql(`update items set done = 1 where id = ?;`, [
-                      id,
-                    ]);
-                  },
-                  null,
-                  forceUpdate
-                )
-              }
-            />
-            <Items
-              done
-              key={`forceupdate-done-${forceUpdateId}`}
-              onPressItem={(id) =>
-                db.transaction(
-                  (tx) => {
-                    tx.executeSql(`delete from items where id = ?;`, [id]);
-                  },
-                  null,
-                  forceUpdate
-                )
-              }
-            />
+ 
+
+            <View style={styles.sectionContainer}>
+              
+              {items.map(({ id, name }) => (
+                <TouchableOpacity
+                  key={id}
+                  onPress={() =>  onClickDelete(id)}
+                  style={{
+                    backgroundColor:   "#1c9963" ,
+                    borderColor: "#000",
+                    borderWidth: 1,
+                    padding: 8,
+                  }}
+                >
+                  <Text style={{ color:  "#000" }}>{id}- {name}</Text>
+                </TouchableOpacity>
+              ))}
+            </View>
+
           </ScrollView>
         </>
      
@@ -161,14 +164,13 @@ const styles = StyleSheet.create({
     flexDirection: "row",
   },
   input: {
-    borderColor: "#4630eb",
-    borderRadius: 4,
-    borderWidth: 1,
-    flex: 1,
-    height: 48,
-    margin: 16,
-    padding: 8,
-  },
+    borderColor: "#2E4053",
+    backgroundColor:"#F8F9F9",
+    borderRadius: 6,
+    borderWidth: 1, 
+    marginBottom: 16,
+    padding: 8, 
+  }, 
   listArea: {
     backgroundColor: "#f0f0f0",
     flex: 1,
